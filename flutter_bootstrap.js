eec85040deb6147cbf1e2dd5d49ca8f5b27a799a
aesthgitm <kemayoranvhatlast14@gmail.com>
2026-05-24 07:45:58 +0000

    Deploying to gh-pages from @ aesthgitm/BharataWayang@e203ec1e633887d0d0554cafa7ec436cd1f87e6e 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1812239481" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "811150154" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
